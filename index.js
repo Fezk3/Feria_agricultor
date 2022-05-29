e38b38a0342076e67b8e548a4a601cb9e6a299a1
Victor Segura <seguravictor57@gmail.com>
2022-05-28 23:49:49 -0600

    notas importantes

--- index.js
+++ index.js
@@ -12,10 +12,35 @@ mongoose.connect('mongodb://localhost:27017/feria', { useNewUrlParser: true, use
         console.log(err)
     })
 
+// para usar ejs como motor de plantillas
 app.set('view engine', 'ejs');
+// para el body de los formularios POST
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'))
+// para que se pueda usar el metodo PUT, DELETE, PATCH
+app.use(methodOverride('_method'));
+// para parsear json
+app.use(express.json());
 
+/*
+
+Notas:
+
+para parsear rutas /:id se usa el metodo req.params
+const {id} = req.params;
+para parsear querystring se usa el metodo req.query 
+const nombre = req.query.nombre;
+para parsear body se usa el metodo req.body -> para los formularios POST
+const nombre = req.body.nombre;
+
+Para enviar resultados de la base de datos a la vista se usa
+res.render('ruta', {datos}); // datos es un objeto 
+
+En plantillas ejs:
+<%  %> -> para escribir codigo javascript
+<%= %> -> para escribir codigo javascript y que se muestre en la vista
+<%- %> -> para includes de plantillas
+
+*/
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
