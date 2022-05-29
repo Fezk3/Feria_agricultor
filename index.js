37b4d830e5afa3d529ac51055382e6e7e5425892
Victor Segura <seguravictor57@gmail.com>
2022-05-28 23:29:16 -0600

    mensajes de conexion a mongo db

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const methodOverride = require('method-override');
 
 mongoose.connect('mongodb://localhost:27017/feria', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
-        console.log("MONGO CONNECTION OPEN!!!")
+        console.log("Connexion a la base de datos exitosa");
     })
     .catch(err => {
-        console.log("OH NO MONGO CONNECTION ERROR!!!!")
+        console.log("Error al conectar a la base de datos: ");
         console.log(err)
     })
 
