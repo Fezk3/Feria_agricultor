fd36d1665026a6c87d23a9e0c05be8264eefda50
Victor Segura <seguravictor57@gmail.com>
2022-06-01 13:01:33 -0600

    js en public

--- public/js/app2.js
+++ public/js/app2.js
@@ -4,7 +4,7 @@ const lis = document.querySelectorAll('li');
 const agregar = document.querySelector('#agregar');
 
 const contadorPuestos = () => {
-    if (h1s.length === 15) {
+    if (lis === 15) {
         agregar.classList.add('disabled');
     } else {
         agregar.classList.remove('disabled');
